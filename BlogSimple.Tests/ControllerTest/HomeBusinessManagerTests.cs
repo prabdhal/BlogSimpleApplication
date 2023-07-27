9dadb629a07b567e9fc20271a749e33e5f5fd21e
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-07-27 00:44:07 -0400

    Added Unit test project

---
+++ BlogSimple.Tests/ControllerTest/HomeBusinessManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using BlogSimple.Model.Models;
+using BlogSimple.Model.Services.Interfaces;
+using FakeItEasy;
+
+namespace BlogSimple.Tests.ControllerTest;
+
+public class HomeBusinessManagerTests
+{
+    private IBlogService _blogService;
+    public HomeBusinessManagerTests()
+    {
+        // Dependencies
+        _blogService = A.Fake<IBlogService>();
+    }
+}
