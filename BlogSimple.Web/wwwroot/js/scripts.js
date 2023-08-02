eefcb8e2fffeef13d6d2ded6e80465c2a6418bc9
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-08-01 23:48:28 -0400

    Users can search via search bar and categories. Search bar is overriden by category selection search.

--- BlogSimple.Web/wwwroot/js/scripts.js
+++ BlogSimple.Web/wwwroot/js/scripts.js
@@ -17,11 +17,100 @@ let blogsToShow = [];
 // pagination values 
 const maxBlogsPerPage = 2;
 let currentPageNumber = 1;
-let totalPageCount = 0;
 const paginationNavContainer = document.querySelector('#paginationNavContainer');
+
+// search string and category
+let blogSearchString = "";
+let blogCategoryIdx = 100;
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+
+
+// sets blogCategoryIdx
+const setBlogCategory = (selectedCategory) => {
+    setBlogSearchString("");
+    searchBarInput.value = "";
+    blogCategoryIdx = getBlogCategoryIdx(selectedCategory);
+    setBlogsToDisplay();
+    displayBlogs();
+    console.log('clicked ' + selectedCategory);
+}
+
+// lists all blog cateogries on side widget
+const setUpBlogCategoryList = () => {
+    blogCategoryData.forEach(cat => {
+        var catName = getBlogCategoryName(cat);
+
+        const listItemElement = document.createElement('li');
+        listItemElement.innerHTML = `<a href="#" onclick="return false;">${catName}</a>`;
+        listItemElement.classList = 'list-select';
+        listItemElement.addEventListener('click', () => setBlogCategory(catName));
+
+        blogCategoryListContainer.append(listItemElement);
+    });
+}
+
+// sets blogSearchString 
+const setBlogSearchString = (searchString) => {
+    blogSearchString = searchString;
+    currentPageNumber = 1;
+}
+
+// sets blogsToShow 
+const setBlogsToDisplay = () => {
+    blogsToShow = [];
+
+    let blogCategory = blogCategoryIdx;
+
+    blogsData.forEach(b => {
+        let blog = JSON.parse(b.getAttribute("value"));
+
+        // exclude featured blogs
+        if (blog.isFeatured) return;
+
+        if (blog.title.toString().toLowerCase().includes(blogSearchString) ||
+            blog.description.toString().toLowerCase().includes(blogSearchString) ||
+            blog.content.toString().toLowerCase().includes(blogSearchString) ||
+            blogSearchString.toString() === "") {
+            if (blog.category == blogCategory || blogCategory === 100) {
+                blogsToShow.push(blog);
+            }
+        }
+    });
+    console.log('blog search string ' + blogSearchString);
+    console.log('blogs to show ' + blogsToShow);
+    console.log('blogs to show length ' + blogsToShow.length);
+    updatePaginationVariables(blogsToShow.length);
+}
+
+const updatePaginationVariables = (blogCount) => {
+    totalPageCount = Math.ceil(blogCount / maxBlogsPerPage);
+}
+
+const setPageNumber = (num) => {
+    if (num > totalPageCount || num < 1) {
+        if (num <= 1) {
+            num = 1;
+            currentPageNumber = 1;
+        }
+        else if (num >= totalPageCount) {
+            num = totalPageCount;
+            currentPageNumber = totalPageCount;
+        }
+    }
+
+    currentPageNumber = num;
+    setBlogsToDisplay();
+    displayBlogs();
+}
+
+// search bar event listener 
+searchBarInput.addEventListener('input', (e) => {
+    setBlogSearchString(e.target.value);
+    setBlogsToDisplay();
+    displayBlogs();
+});
 
 // maps enum int to its name
 const getBlogCategoryName = (value) => {
@@ -44,45 +133,35 @@ const getBlogCategoryName = (value) => {
             return 'Freebies';
         case 8:
             return 'Other';
+        default:
+            return 'All';
     }
 }
 
-// displays blogs by search text
-const getAllBlogsBySearch = (searchString) => {
-    console.log("search string: " + searchString);
-    let blogsToShow = [];
-
-    blogsData.forEach(b => {
-        var blog = JSON.parse(b.getAttribute("value"));
-        
-        if (blog.title.toString().toLowerCase().includes(searchString) ||
-            blog.description.toString().toLowerCase().includes(searchString) ||
-            blog.content.toString().toLowerCase().includes(searchString) || 
-            searchString.toString() === "") {
-            if (blog.isFeatured) return;
-            blogsToShow.push(blog);
-        }
-    });
-    displayBlogs(blogsToShow);
-}
-
-// displays blogs by selected category on side widget
-const getBlogsFromSelectedCategory = (catName) => {
-    console.log("clicked selected cat: " + catName);
-    blogsToShow = [];
-
-    blogsData.forEach(b => {
-        var blog = JSON.parse(b.getAttribute("value"));
-        var cat = getBlogCategoryName(blog.category);
-
-        console.log('cat: ' + cat);
-        console.log('cat Name: ' + catName);
-        if (cat == catName) {
-            if (blog.isFeatured) return;
-            blogsToShow.push(blog);
-        }
-    });
-    displayBlogs(blogsToShow);
+// maps enum name into ints
+const getBlogCategoryIdx = (value) => {
+    switch (value) {
+        case 'HTML':
+            return 0;
+        case 'CSS':
+            return 1;
+        case 'JavaScript':
+            return 2;
+        case 'C#':
+            return 3;
+        case 'Object-Oriented Programming':
+            return 4;
+        case 'Web Design':
+            return 5;
+        case 'Tutorials':
+            return 6;
+        case 'Freebies':
+            return 7;
+        case 'Other':
+            return 8;
+        default:
+            return 100;
+    }
 }
 
 // displays the featured blog 
@@ -113,18 +192,16 @@ const displayFeaturedBlog = () => {
 }
 
 // displays all blogs 
-const displayBlogs = (blogs) => {
-
-    createPagination(blogs);
+const displayBlogs = () => {
+    createPagination();
 
     blogsDisplayContainer.innerHTML = '';
 
     let curBlogIdx = (currentPageNumber - 1) * maxBlogsPerPage;
     let blogIdxOnCurrentPage = currentPageNumber * maxBlogsPerPage;
 
-    if (blogs.length <= 0) {
+    if (blogsToShow.length <= 0) {
         // no results
-
         const divElement = document.createElement('div');
         divElement.innerHTML =
             `<div class="col-lg-6">
@@ -137,10 +214,10 @@ const displayBlogs = (blogs) => {
 
     for (var i = curBlogIdx; i < blogIdxOnCurrentPage; i++) {
 
-        if (blogs[i] == null) return;
-        if (blogs[i].isFeatured) return;
+        if (blogsToShow[i] == null) return;
+        if (blogsToShow[i].isFeatured) return;
 
-        var createdOnDate = new Date(blogs[i].createdOn);
+        var createdOnDate = new Date(blogsToShow[i].createdOn);
         var year = createdOnDate.getFullYear();
         var month = months[createdOnDate.getMonth()].substring(0, 3);
         var date = createdOnDate.getDate();
@@ -150,13 +227,13 @@ const displayBlogs = (blogs) => {
         divElement.innerHTML =
            `<div class="col-lg-6">
                 <div class="card mb-4">
-                    <a href="/Home/Details/${blogs[i].id}">
+                    <a href="/Home/Details/${blogsToShow[i].id}">
                         <img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." />
                     </a>
                     <div class="card-body">
                         <div class="small text-muted">${month} ${date}, ${year}</div>
-                        <h2 class="card-title h4"><a href="/Home/Details/${blogs[i].id}">${blogs[i].title}</a></h2>
-                        <p class="card-text">${blogs[i].description}</p>
+                        <h2 class="card-title h4"><a href="/Home/Details/${blogsToShow[i].id}">${blogsToShow[i].title}</a></h2>
+                        <p class="card-text">${blogsToShow[i].description}</p>
                     </div>
                 </div>
             </div>`;
@@ -165,28 +242,13 @@ const displayBlogs = (blogs) => {
     }
 }
 
-// lists all blog cateogries on side widget
-const setUpBlogCategoryList = () => {
-    blogCategoryData.forEach(cat => {
-        var catName = getBlogCategoryName(cat);
-
-        const listItemElement = document.createElement('li');
-        listItemElement.innerHTML = `<a href="#" onclick="return false;">${catName}</a>`;
-        listItemElement.classList = 'list-select';
-        listItemElement.addEventListener('click', () => getBlogsFromSelectedCategory(catName));
-
-        blogCategoryListContainer.append(listItemElement);
-    });
-}
-
 // handles pagination and creates/designs the pagination nav accordingly
-const createPagination = (blogs) => {
+const createPagination = () => {
     paginationNavContainer.innerHTML = '';
     // do pagination only if greater than 10 blogs
-    if (blogs.length > maxBlogsPerPage) {
+    if (blogsToShow.length > maxBlogsPerPage) {
 
-        totalPageCount = Math.ceil(blogs.length / maxBlogsPerPage); 
-        console.log('blogs length: ' + blogs.length);
+        console.log('blogs length: ' + blogsToShow.length);
         console.log('max blogs: ' + maxBlogsPerPage);
         console.log('total page count: ' + totalPageCount);
 
@@ -198,7 +260,7 @@ const createPagination = (blogs) => {
 
         let prevBtnElement = document.createElement('li');
         prevBtnElement.classList = "page-item";
-        prevBtnElement.addEventListener('click', () => setAsCurrentPage(--currentPageNumber));
+        prevBtnElement.addEventListener('click', () => setPageNumber(--currentPageNumber));
 
         if (currentPageNumber == 1) {
             prevBtnElement.innerHTML = `<a class="page-link disabled">Newer</a>`;
@@ -210,7 +272,7 @@ const createPagination = (blogs) => {
         for (let i = 0; i < totalPageCount; i++) {
             let liElement = document.createElement('li');
             liElement.classList = "page-item";
-            liElement.addEventListener('click', () => setAsCurrentPage(i + 1));
+            liElement.addEventListener('click', () => setPageNumber(i + 1));
             if (currentPageNumber == (i + 1)) {
                 liElement.innerHTML = `<a class="page-link font-weight-bold">${i + 1}</a>`;
             } else {
@@ -222,7 +284,7 @@ const createPagination = (blogs) => {
 
         let nextBtnElement = document.createElement('li');
         nextBtnElement.classList = "page-item";
-        nextBtnElement.addEventListener('click', () => setAsCurrentPage(++currentPageNumber));
+        nextBtnElement.addEventListener('click', () => setPageNumber(++currentPageNumber));
 
         if (currentPageNumber == totalPageCount) {
             nextBtnElement.innerHTML = `<a class="page-link disabled">Older</a>`;
@@ -235,31 +297,6 @@ const createPagination = (blogs) => {
         paginationNavContainer.append(ulElement);
     }
 }
-
-// on click event for pagination buttons
-const setAsCurrentPage = (pageNumber) => {
-    if (pageNumber > totalPageCount || pageNumber < 1) {
-        if (pageNumber <= 1) {
-            pageNumber = 1;
-            currentPageNumber = 1;
-        }
-        if (pageNumber >= totalPageCount) {
-            pageNumber = totalPageCount;
-            currentPageNumber = totalPageCount;
-        }
-
-        return;
-    } 
-
-    currentPageNumber = pageNumber;
-    console.log(currentPageNumber);
-    getAllBlogsBySearch("");
-}
-
-// search bar event listener 
-searchBarInput.addEventListener('input', (e) => {
-    getAllBlogsBySearch(e.target.value);
-})
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -279,7 +316,82 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-
+setBlogCategory("All");
+setBlogSearchString("")
 setUpBlogCategoryList();
-getAllBlogsBySearch("");
+setBlogsToDisplay();
+displayBlogs();
 displayFeaturedBlog();
+
+
+
+
+
+
+
+
+
+//// displays blogs by search text
+//const getAllBlogsBySearch = (searchString) => {
+//    blogSearchString = searchString;
+
+//    let catName = getBlogCategoryName(blogCategoryIdx);
+
+//    blogsToShow = [];
+
+//    blogsData.forEach(b => {
+//        var blog = JSON.parse(b.getAttribute("value"));
+
+//        if (blog.isFeatured) return;
+//        if (blog.title.toString().toLowerCase().includes(blogSearchString) ||
+//            blog.description.toString().toLowerCase().includes(blogSearchString) ||
+//            blog.content.toString().toLowerCase().includes(blogSearchString) ||
+//            blogSearchString.toString() === "") {
+//            if (blog.category == catName || catName == 'All') {
+//                blogsToShow.push(blog);
+//            }
+//        }
+//    });
+//    displayBlogs(blogsToShow);
+//}
+
+//// displays blogs by selected category on side widget
+//const getBlogsFromSelectedCategory = (catName) => {
+//    console.log("clicked selected cat: " + catName);
+//    blogCategoryIdx = getBlogCategoryIdx(catName);
+//    blogSearchString = "";
+//    currentPageNumber = 1;
+
+//    blogsToShow = [];
+
+//    blogsData.forEach(b => {
+//        var blog = JSON.parse(b.getAttribute("value"));
+//        var cat = getBlogCategoryName(blog.category);
+
+//        if (blog.isFeatured) return;
+
+//        if (cat == getBlogCategoryName(blogCategoryIdx)) {
+//            blogsToShow.push(blog);
+//        }
+//    });
+//    displayBlogs(blogsToShow);
+//}
+
+// on click event for pagination buttons
+//const setAsCurrentPage = (pageNumber) => {
+//    if (pageNumber > totalPageCount || pageNumber < 1) {
+//        if (pageNumber <= 1) {
+//            pageNumber = 1;
+//            currentPageNumber = 1;
+//        }
+//        if (pageNumber >= totalPageCount) {
+//            pageNumber = totalPageCount;
+//            currentPageNumber = totalPageCount;
+//        }
+//        return;
+//    } 
+
+//    currentPageNumber = pageNumber;
+//    console.log(currentPageNumber);
+//    getAllBlogsBySearch(blogSearchString);
+//}
