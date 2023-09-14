af44c34fdddb7d7ff6a2c4d0f226a06d05bd1093
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-09-14 11:10:46 -0400

    Roles and users can be viewed by admin in table format. Updated view for reset and forgot password.

--- BlogSimple.Model/ViewModels/AdminViewModels/MyAdminViewModel.cs
+++ BlogSimple.Model/ViewModels/AdminViewModels/MyAdminViewModel.cs
@@ -7,4 +7,11 @@ public class MyAdminViewModel
     public User AccountUser { get; set; }
     public List<UserRole> UserRoles { get; set; }
     public UserRole UserRole { get; set; } = new UserRole();
+    public List<User> RegisteredUsers { get; set; }
+    public Dictionary<Guid, string> RoleNameMapping { get; set; } = new Dictionary<Guid, string>
+    {
+        { new Guid("34ad0d02-7521-49ff-8a55-bb75e50224bb"), "Admin"},
+        { new Guid("ae5c1c60-4940-4623-ba3d-9468f58b38a6"), "Verified User"},
+        { new Guid("4c2652f9-62a7-44b9-8f5e-975eb92cd71b"), "Unverified User"},
+    };
 }
--- BlogSimple.Web/BusinessManager/AdminBusinessManager.cs
+++ BlogSimple.Web/BusinessManager/AdminBusinessManager.cs
@@ -12,26 +12,31 @@ public class AdminBusinessManager : IAdminBusinessManager
 {
     private UserManager<User> _userManager;
     private readonly IRoleService _roleService;
+    private readonly IUserService _userService;
 
     public AdminBusinessManager(
         UserManager<User> userManager,
-        IRoleService roleService
+        IRoleService roleService,
+        IUserService userService
         )
     {
-        _roleService = roleService;
         _userManager = userManager;
+        _roleService = roleService;
+        _userService = userService;
     }
 
     public async Task<MyAdminViewModel> GetMyAdminViewModel(ClaimsPrincipal claimsPrincipal)
     {
         var user = await _userManager.GetUserAsync(claimsPrincipal);
         var roles = await _roleService.GetAll();
+        var users = await _userService.GetAll();
 
         return new MyAdminViewModel
         {
             AccountUser = user,
             UserRole = new UserRole(),
-            UserRoles = roles
+            UserRoles = roles,
+            RegisteredUsers = users
         };
     }
 
