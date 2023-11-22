186410fa404065257d6ca2179898fe28244a4230
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-11-22 11:42:18 -0500

    Update AccountController.cs

--- BlogSimple.Web/Controllers/AccountController.cs
+++ BlogSimple.Web/Controllers/AccountController.cs
@@ -92,24 +92,28 @@ namespace BlogSimple.Web.Controllers
                 if (user != null)
                 {
                     Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user, password, false, false);
-                    if (result.Succeeded || user.EmailConfirmed == false)
+                    if (result.Succeeded && user.EmailConfirmed == true)
                     {
-                        if (user.EmailConfirmed == false)
-                        {
-                            EmailConfirmViewModel model = new EmailConfirmViewModel
-                            {
-                                Email = user.Email,
-                                EmailSent = true,
-                                EmailVerified = user.EmailConfirmed
-                            };
-                            return RedirectToAction("EmailVerification", model);
-                        }
                         return Redirect(returnurl ?? "/Post/Index");
+                    }
+                    else if (result.Succeeded && user.EmailConfirmed == false)
+                    {
+                        EmailConfirmViewModel model = new EmailConfirmViewModel
+                        {
+                            Email = user.Email,
+                            EmailSent = true,
+                            EmailVerified = user.EmailConfirmed
+                        };
+                        return RedirectToAction("EmailVerification", model);
                     }
                     else
                     {
                         ModelState.AddModelError("", "Invalid credentials");
                     }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User does not exist");
                 }
             }
 
